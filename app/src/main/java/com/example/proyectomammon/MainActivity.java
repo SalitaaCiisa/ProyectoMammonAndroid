bb0faeebc0ef33c07e548d7b84899943de54de5e
SalitaaCiisa <alonso.salas.inostroza@ciisa.cl>
2022-09-21 19:50:18 -0300

    Primer commit puse que añadieran todos los archivos menos los .gitignore

---
+++ app/src/main/java/com/example/proyectomammon/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.proyectomammon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.buttonLogin);
+    }
+
+    public void cambioActividad(View view){
+        Intent i = new Intent(getApplicationContext(), crudCuentasBancarias.class);
+        startActivity(i);
+    }
+}
